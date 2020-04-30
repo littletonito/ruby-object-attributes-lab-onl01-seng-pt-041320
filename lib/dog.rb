6d3d928b781547b79dddc500be10001e4d55ca12
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-30 01:51:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+
+
+class Dog
+  
+  def name= (doggie)
+    @name = doggie
+  end
+  
+  
+  def name
+    @name
+  end
+  
+  def breed= (dog)
+    @breed = dog
+  end
+  
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+
+
+class Person
+  
+  def name= (human)
+    @name = human
+  end
+
+  def name 
+    @name
+  end
+  
+  
+  
+  def job= (work)
+    @job = work
+  end
+
+  def job
+    @job
+  end
+  
+end
